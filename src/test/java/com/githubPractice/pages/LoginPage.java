96f25e30724d960f46ce1940ccc05a4b2973c53c
Fatih Beyhan <fbeyhan@gmail.com>
2019-11-18 11:40:14 -0500

    first commit

---
+++ src/test/java/com/githubPractice/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.githubPractice.pages;
+
+import org.junit.Test;
+
+public class LoginPage {
+
+    @Test
+    public void test1(){
+        System.out.println("github practice commit number 1");
+    }
+    
+}
